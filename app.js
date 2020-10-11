e6ba658fcd7143c08e801e3b00508bdd82372974
PEDSF <pe.dsf@free.fr>
2020-10-11 10:59:28 +0200

    findOne and find implementation

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const app = express();
+const Thing = require('./models/Thing.js');
 
 mongoose.connect('mongodb+srv://MongoDBUser:XTFu8ZyEOnwt39MF@mongodbcluster.ebujh.mongodb.net/test?retryWrites=true&w=majority',
     { useNewUrlParser: true,
         useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -19,33 +21,33 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Objet créé !'
+app.post('/api/stuff', (req, res) => {
+    delete req.body._id;
+
+    // récupère l'objet passé dans le body de la requète
+    const thing = new Thing({
+        ...req.body
     });
+
+    // utilise la méthode de l'objet pour le sauver
+    thing.save()
+        .then(() => res.status(201).json({message: 'Objet enregistré !'}))
+        .catch(error  => res.status(400).json({error }));
 });
 
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    res.status(200).json(stuff);
+app.get('/api/stuff/:id', (req, res) => {
+    // recherche l'élément par son id passé en paramètre
+    Thing.findOne({_id: req.params.id})
+       .then(thing => res.status(200).json(thing))
+       .catch(error => res.status(404).json({ error}));
 });
+
+app.get('/api/stuff', (req, res) => {
+    // recherche les éléments
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error}));
+});
+
 
 module.exports = app;
